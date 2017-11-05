a920807c532f2cb19f5157f66dc7049380ab24d8
wlan0 <sidharthamn@gmail.com>
2017-11-05 08:44:15 -0800

    wire into output

--- cmd/root_cmd.go
+++ cmd/root_cmd.go
@@ -1,11 +1,16 @@
 package cmd
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"strings"
 
+	"github.com/ghodss/yaml"
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 
+	"github.com/koki/short/converter"
 	"github.com/koki/short/parser"
 	"github.com/koki/short/util"
 )
@@ -47,21 +52,27 @@ Full documentation available at https://docs.koki.io/short
 	kubeNative bool
 	// filenames holds the input files that are to be converted to shorthand or kuberenetes native syntax
 	filenames []string
-	// output denotes the destination of the convereted data
+	// output denotes the destination of the converted data
 	output string
+	// silent denotes that the conversion output should not be printed to stdout
+	silent bool
 )
 
 func init() {
 	// local flags to root command
 	RootCmd.Flags().BoolVarP(&kubeNative, "kube-native", "k", false, "convert to kube-native syntax")
 	RootCmd.Flags().StringSliceVarP(&filenames, "filenames", "f", nil, "path or url to input files to read manifests")
-	RootCmd.Flags().StringVarP(&output, "output", "o", "", "output to filename instead of stdin")
+	RootCmd.Flags().StringVarP(&output, "output", "o", "yaml", "output format (yaml*|json)")
+	RootCmd.Flags().BoolVarP(&silent, "silent", "s", false, "silence output to stdout")
 
 	// parse the go default flagset to get flags for glog and other packages in future
 	RootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 
 	// defaulting this to true so that logs are printed to console
 	flag.Set("logtostderr", "true")
+
+	//suppress the incorrect prefix in glog output
+	flag.CommandLine.Parse([]string{})
 
 	RootCmd.AddCommand(versionCmd)
 	//RootCmd.AddCommand(manCommand)
@@ -85,6 +96,10 @@ func short(c *cobra.Command, args []string) error {
 		}
 	}
 
+	if strings.ToLower(output) != "yaml" && strings.ToLower(output) != "json" {
+		return util.UsageErrorf("unexpected value %s for -o --output", output)
+	}
+
 	useStdin := false
 
 	if len(args) == 1 && args[0] == "-" {
@@ -96,10 +111,46 @@ func short(c *cobra.Command, args []string) error {
 	glog.V(3).Info("parsing input data")
 	data, err := parser.Parse(filenames, useStdin)
 	if err != nil {
-		return util.UsageErrorf(c.CommandPath(), err)
+		return err
 	}
 
-	glog.Errorf("len=%d %#v", len(data), data)
+	var convertedData interface{}
+
+	if kubeNative {
+		glog.V(3).Info("converting input to kubernetes native syntax")
+		convertedData, err = converter.ConvertToKubeNative(data)
+		if err != nil {
+			return err
+		}
+	} else {
+		glog.V(3).Info("converting input to koki native syntax")
+		convertedData, err = converter.ConvertToKokiNative(data)
+		if err != nil {
+			return err
+		}
+	}
+
+	if silent {
+		return nil
+	}
+
+	var outputData []byte
+
+	if strings.ToLower(output) == "yaml" {
+		glog.V(3).Info("marshalling converted data into yaml")
+		outputData, err = yaml.Marshal(convertedData)
+		if err != nil {
+			return err
+		}
+	} else {
+		glog.V(3).Info("marshalling converted data into json")
+		outputData, err = json.MarshalIndent(convertedData, " ", "  ")
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("%s\n", outputData)
 
 	return nil
 }
