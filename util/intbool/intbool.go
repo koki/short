9b0881d826f7584e7435001eea9b185ba0c5c4ed
Kynan Rilee <kynan.rilee@gmail.com>
2017-11-16 10:42:42 -0800

    support numbers in Container Args

--- util/intbool/intbool.go
+++ util/intbool/intbool.go
@@ -21,13 +21,9 @@ type Type int
 
 const (
 	Int  Type = iota // The IntOrBool holds an int.
-	Bool             // The IntOrBool holds a string.
+	Bool             // The IntOrBool holds a bool.
 )
 
-// FromInt creates an IntOrBool object with an int32 value. It is
-// your responsibility not to call this method with a value greater
-// than int32.
-// TODO: convert to (val int32)
 func FromInt(val int) *IntOrBool {
 	if val > math.MaxInt32 || val < math.MinInt32 {
 		glog.Errorf("value: %d overflows int32\n%s\n", val, debug.Stack())
@@ -35,7 +31,6 @@ func FromInt(val int) *IntOrBool {
 	return &IntOrBool{Type: Int, IntVal: int32(val)}
 }
 
-// FromString creates an IntOrBool object with a string value.
 func FromBool(val bool) *IntOrBool {
 	return &IntOrBool{Type: Bool, BoolVal: val}
 }
