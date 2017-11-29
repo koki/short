bc14b62e7a0196d8570c2c34092f24867058f95e
Kynan Rilee <kynan.rilee@gmail.com>
2017-11-29 14:47:10 -0800

    basic chrome extension functionality

--- chrome-extension/src/content-script/background.js
+++ chrome-extension/src/content-script/background.js
@@ -1,18 +1,42 @@
 chrome.runtime.onConnect.addListener(function(port) {
-  //var tab = port.sender.tab;
+    //var tab = port.sender.tab;
+    function sendRequest(body) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:8080/convert', true);
+        xhr.setRequestHeader('Content-Type', 'application/yaml');
+        xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://localhost:8080/');
+        xhr.onload = (e) => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    port.postMessage({
+                        body: xhr.responseText
+                    });
+                } else {
+                    port.postMessage({
+                        error: {
+                            status: xhr.status,
+                            statusText: xhr.statusText,
+                            text: xhr.responseText
+                        }
+                    });
+                }
+            }
+        };
+        xhr.onerror = (e) => {
+            console.error({
+                status: xhr.status,
+                statusText: xhr.statusText,
+                text: xhr.responseText
+            });
+        };
 
-  port.onMessage.addListener(function(info) {
-    var xhr = new XMLHttpRequest();
-    var body = info.file
-   
-    console.log(body)
-
-    xhr.open('POST', 'http://localhost:8080/convert', false)
-    xhr.setRequestHeader('Content-Type', "application/yaml")
-    xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://localhost:8080/')
-    xhr.send(JSON.stringify(body))
-    xhr.onload = function() {
-      port.postMessage(xhr.ResponseText)
+        xhr.send(body);
     }
-  });
+
+    port.onMessage.addListener(function(info) {
+        if (info.fileLines) {
+            let body = info.fileLines.join('\n');
+            sendRequest(body);
+        }
+    });
 });
--- chrome-extension/src/content-script/index.js
+++ chrome-extension/src/content-script/index.js
@@ -1,54 +1,128 @@
 import githubInjection from 'github-injection';
 import ghPageType from 'github-page-type';
-import $ from "jquery";
+import $ from 'jquery';
+import _ from 'lodash';
 
 githubInjection(window, err => {
-    !err
-    && ghPageType(window.location.href, ghPageType.REPOSITORY_BLOB)
-    && $(".final-path").text().endsWith(".yaml")
-    && $("#toggle-yaml").length <= 0
-    && renderYAMLToggleButton();
+    if (!err &&
+        ghPageType(window.location.href, ghPageType.REPOSITORY_BLOB) &&
+        $('.final-path').text().endsWith('.yaml') &&
+        $('#toggle-yaml').length <= 0 &&
+        isKubeYaml(getLinesFromRows(getFileRows()))) {
+        main();
+    }
 });
 
-chrome.runtime.onConnect.addListener(function(port) {
-    port.onMessage.addListener(function(msg) {
-        updateFileWithInfo(msg.file-info, msg.file-data);
+function main() {
+    let toggleButton = renderYAMLToggleButton();
+    let kubeRows = getFileRows();
+    let kubeLines = getLinesFromRows(kubeRows);
+
+    let kokiRows = new Promise((resolve, reject) => {
+        sendFileContents(kubeLines, (content) => {
+            resolve(buildRowsForContent(content));
+        }, reject);
     });
-});
+
+    kokiRows.catch((error) => {
+        // TODO: Find a good way to present the error to the user.
+        toggleButton.detach();
+        console.error(error);
+    });
+
+    var toggleDisabled = false;
+    toggleButton.click(() => {
+        if (toggleDisabled) {
+            return;
+        }
+        if (toggleButton.text() === 'Pretty YAML') {
+            toggleButton.text('Working...');
+            toggleDisabled = true;
+            kokiRows.then((kokiRows) => {
+                toggleDisabled = false;
+                replaceFileRows(kokiRows);
+                toggleButton.text('Kube YAML');
+            });
+        } else {
+            replaceFileRows(kubeRows);
+            toggleButton.text('Pretty YAML');
+        }
+    });
+}
+
+function isKubeYaml(lines) {
+    var hasApiVersion, hasKind;
+    _.forEach(lines, (line) => {
+        hasApiVersion |= line.startsWith('apiVersion:');
+        hasKind |= line.startsWith('kind:');
+    });
+    return hasApiVersion && hasKind;
+}
+
+function buildRowForLine(lineNumber, line) {
+    return `
+        <tr>
+            <td id="L${lineNumber}" class="blob-num js-line-number" data-line-number="${lineNumber}"></td>
+            <td id="LC${lineNumber}" class="blob-code blob-code-inner js-file-line">${line}</td>
+        </tr>`;
+}
+
+function buildRowsForContent(content) {
+    let lines = content.split('\n');
+
+    // Strip the last line if it's empty.
+    if (!_.last(lines)) {
+        lines.pop();
+    }
+    return _.map(lines, (line, lineIndex) => buildRowForLine(lineIndex + 1, line));
+}
+
+function getFileRows() {
+    return $('.file .blob-wrapper tr');
+}
+
+function replaceFileRows(rows) {
+    let tbody = $('.file .blob-wrapper table tbody');
+    let oldRows = tbody.find('tr');
+    oldRows.detach();
+    tbody.append(rows);
+
+    return oldRows;
+}
+
+function getLinesFromRows(rows) {
+    return _.map(rows.find('td.blob-code.blob-code-inner.js-file-line'), (line) => line.textContent);
+}
 
 function updateFileWithInfo(fileInfo, fileData) {
-    $("#pretty-yaml-info").text(fileInfo);
-    $("#pretty-yaml-data").text(fileData);
+    $('#pretty-yaml-info').text(fileInfo);
+    $('#pretty-yaml-data').text(fileData);
+}
+
+function sendFileContents(lines, onSuccess, onError) {
+    let port = chrome.runtime.connect();
+    port.postMessage({
+        fileLines: lines
+    });
+    port.onMessage.addListener(function(msg) {
+        if (msg.error) {
+            onError(msg.error);
+            return;
+        }
+
+        onSuccess(msg.body);
+    });
 }
 
 function renderYAMLToggleButton() {
-    let fileElement = $(".file");
-    let prettyYAMLFileInfo = $("<div />", {id: "pretty-yaml-info"}).hide().appendTo(fileElement.find(".file-header"));
-    let prettyYAMLFileData = $("<table />", {id: "pretty-yaml-data"}).hide().appendTo(fileElement.find(".blob-wrapper"));
-    let codeElementFileInfo = fileElement.find(".file-info");
-    let codeElementFileData = fileElement.find(".blob-wrapper");
-    let rawButton = fileElement.find("#raw-url");
+    let rawButton = $('#raw-url');
+
     let toggleButton = rawButton
         .clone()
-        .text("Pretty YAML")
-        .attr("href", "#")
-        .attr("id", "toggle-yaml")
-        .insertBefore(rawButton)
-        .click(() => {
-            if (toggleButton.text() === "Pretty YAML") {
-                codeElementFileInfo.hide();
-                codeElementFileData.hide();
-                prettyYAMLFileInfo.show();
-                prettyYAMLFileData.show();
-                chrome.runtime.connect().
-                    postMessage(codeElementFileData.text());
-                toggleButton.text("Kube YAML");
-            } else {
-                prettyYAMLFileInfo.hide();
-                prettyYAMLFileData.hide();
-                codeElementFileInfo.show();
-                codeElementFileData.show();
-                toggleButton.text("Pretty YAML");
-            }
-        });
+        .text('Pretty YAML')
+        .attr('href', '#')
+        .attr('id', 'toggle-yaml')
+        .insertBefore(rawButton);
+
+    return toggleButton;
 }
