f76a14b1f5ca110def424b7358c886aecdaf64c3
Kynan Rilee <kynan.rilee@gmail.com>
2017-12-01 08:53:15 -0800

    server: stripe integration, remove test keys

--- server/cmd/root_cmd.go
+++ server/cmd/root_cmd.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"time"
 
 	//	"github.com/denizeren/dynamostore"
 	"github.com/golang/glog"
@@ -18,6 +20,8 @@ import (
 	"github.com/koki/short/parser"
 	"github.com/rs/cors"
 	"github.com/spf13/cobra"
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/sub"
 	"golang.org/x/oauth2"
 )
 
@@ -46,6 +50,47 @@ Full documentation available at https://docs.koki.io/short
 	oauthCfg *oauth2.Config
 )
 
+type Env struct {
+	CookieAuthenticationKey string
+	StripeKey               string
+	GithubClientID          string
+	GithubClientSecret      string
+}
+
+const (
+	envCookieAuthKey      = "COOKIE_AUTH_KEY"
+	envStripeKey          = "STRIPE_KEY"
+	envGithubClientID     = "GITHUB_CLIENT_ID"
+	envGithubClientSecret = "GITHUB_CLIENT_SECRET"
+)
+
+func envFromEnv() Env {
+	env := Env{
+		CookieAuthenticationKey: os.Getenv(envCookieAuthKey),
+		StripeKey:               os.Getenv(envStripeKey),
+		GithubClientID:          os.Getenv(envGithubClientID),
+		GithubClientSecret:      os.Getenv(envGithubClientSecret),
+	}
+
+	if len(env.CookieAuthenticationKey) == 0 {
+		glog.Fatalf("missing %s", envCookieAuthKey)
+	}
+
+	if len(env.StripeKey) == 0 {
+		glog.Fatalf("missing %s", envStripeKey)
+	}
+
+	if len(env.GithubClientID) == 0 {
+		glog.Fatalf("missing %s", envGithubClientID)
+	}
+
+	if len(env.GithubClientSecret) == 0 {
+		glog.Fatalf("missing %s", envGithubClientSecret)
+	}
+
+	return env
+}
+
 func init() {
 	// local flags to root command
 	RootCmd.Flags().IntVarP(&port, "port", "p", 8080, "the port on which the server should listen")
@@ -62,11 +107,13 @@ func init() {
 
 	RootCmd.AddCommand(versionCmd)
 
-	store = sessions.NewCookieStore([]byte("dummy-secret"))
-
+	// Keys setup
+	env := envFromEnv()
+	store = sessions.NewCookieStore([]byte(env.CookieAuthenticationKey))
+	stripe.Key = env.StripeKey
 	oauthCfg = &oauth2.Config{
-		ClientID:     "1daa51bf202f482a44b2",
-		ClientSecret: "8af958d8c1c846f8270ee1c0a7e43e3881f6022d",
+		ClientID:     env.GithubClientID,
+		ClientSecret: env.GithubClientSecret,
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  "https://github.com/login/oauth/authorize",
 			TokenURL: "https://github.com/login/oauth/access_token",
@@ -89,6 +136,87 @@ func serve(c *cobra.Command, args []string) error {
 	}
 
 	return s.ListenAndServe()
+}
+
+// TODO: What's the right way to associate a Stripe customer ID with our user?
+//       Keeping it in the sessions DB might not work.
+func getStripeCustomerID(sesh *sessions.Session) string {
+	if id, ok := sesh.Values["stripe_id"]; ok {
+		if id, ok := id.(string); ok {
+			return id
+		}
+
+		glog.Errorf("%v contains non-string stripe_id", sesh.Values)
+	}
+
+	return ""
+}
+
+// TODO: Plan from env.
+func getStripeSubscription(stripeCustomerID string) *stripe.Sub {
+	if len(stripeCustomerID) == 0 {
+		glog.Info("no stripe customer ID, so no stripe subscription")
+		return nil
+	}
+
+	i := sub.List(&stripe.SubListParams{
+		Customer: stripeCustomerID,
+		Plan:     "test_plan_0",
+	})
+
+	for i.Next() {
+		s := i.Sub()
+		glog.Infof("got stripe subscription %v for %s", s, stripeCustomerID)
+		return s
+	}
+
+	glog.Infof("no stripe subscription for %s", stripeCustomerID)
+
+	return nil
+}
+
+// Returns false if subscription already ended or does not exist.
+func setExpiryFromSubscription(sesh *sessions.Session, sub *stripe.Sub) bool {
+	if sub == nil {
+		glog.Info("user doesn't have a subscription")
+		return false
+	}
+	sesh.Values["subscription_end"] = sub.PeriodEnd
+
+	return sub.PeriodEnd > time.Now().Unix()
+}
+
+func checkSubscription(sesh *sessions.Session) bool {
+	if id, ok := sesh.Values["id"]; ok {
+		// Do we need to check with Stripe right now?
+		if subEnd, ok := sesh.Values["subscription_end"]; ok {
+			if subEnd, ok := subEnd.(int64); ok {
+				if subEnd > time.Now().Unix() {
+					// We checked the subscription earlier, and it hasn't expired yet.
+					glog.Infof("previously verified subscription for github ID %v", id)
+					return true
+				}
+			}
+		}
+
+		// Check with Stripe.
+		customerID := getStripeCustomerID(sesh)
+		if len(customerID) == 0 {
+			glog.Infof("no stripe customer ID for github ID %v", id)
+			return false
+		}
+
+		sub := getStripeSubscription(customerID)
+		if sub == nil {
+			glog.Infof("no subscription for stripe customer ID %s, github ID %v", customerID, id)
+			return false
+		}
+
+		return setExpiryFromSubscription(sesh, sub)
+	}
+
+	glog.Info("session has no github ID, so can't look for stripe subscriptions")
+	return false
 }
 
 func login(rw http.ResponseWriter, r *http.Request) {
@@ -119,18 +247,21 @@ func convert(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	id, ok := sesh.Values["id"]
-
-	if sesh.IsNew || !ok {
+	_, hasID := sesh.Values["id"]
+	if sesh.IsNew || !hasID {
 		http.Error(rw, "unauthorized", http.StatusUnauthorized)
 		return
 	}
 
-	eligible := checkEligibility(id.(int))
-	if !eligible {
-		http.Error(rw, err.Error(), http.StatusUnauthorized)
-		return
-	}
+	// TODO: Don't ignore eligibility once subscription system is in place.
+	_ = checkSubscription(sesh)
+	sesh.Save(r, rw)
+	/*
+		if !eligible {
+			http.Error(rw, "no eligible subscription", http.StatusUnauthorized)
+			return
+		}
+	*/
 
 	if r.Method != http.MethodPost {
 		http.Error(rw, "", http.StatusMethodNotAllowed)
@@ -215,8 +346,4 @@ func githubCallback(rw http.ResponseWriter, r *http.Request) {
 	session.Save(r, rw)
 
 	fmt.Fprint(rw, closingPage)
-}
-
-func checkEligibility(id int) bool {
-	return true
 }
