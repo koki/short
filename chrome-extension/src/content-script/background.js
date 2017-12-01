a12fa150b6d9b1fb88d72633ca47bbf0dd64a657
Kynan Rilee <kynan.rilee@gmail.com>
2017-12-01 12:45:37 -0800

    sign in on loading the extension. retry on 401 from convert/

--- chrome-extension/src/content-script/background.js
+++ chrome-extension/src/content-script/background.js
@@ -8,9 +8,9 @@ function sendRequest(body, onSuccess, onError) {
     xhr.onload = (e) => {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                onSuccess(xhr.responseText);
+                onSuccess && onSuccess(xhr.responseText);
             } else {
-                onError({
+                onError && onError({
                     status: xhr.status,
                     statusText: xhr.statusText,
                     text: xhr.responseText
@@ -28,6 +28,35 @@ function sendRequest(body, onSuccess, onError) {
 
     xhr.send(body);
 }
+
+function checkCookies(onSuccess, onFailure) {
+    chrome.cookies.get({
+        url: serverURL,
+        name: 'user'
+    }, (result) => {
+        console.log(result);
+        if (result) {
+            onSuccess && onSuccess();
+        } else {
+            onFailure && onFailure();
+        }
+    });
+}
+
+function githubSignIn(onSuccess, onFailure) {
+    const loginWindow = window.open(`${serverURL}/login`);
+    const checkWindowClosed = setInterval(() => {
+        if (!loginWindow || loginWindow.closed) {
+            clearInterval(checkWindowClosed);
+            checkCookies(onSuccess, onFailure);
+        }
+    }, 200);
+}
+
+// On loading the extension, try to sign in.
+checkCookies(null, githubSignIn(null, () => console.error({
+    text: "sign-in tab closed, but no cookie found"
+})));
 
 chrome.runtime.onConnect.addListener(function(port) {
     //let tab = port.sender.tab;
@@ -50,45 +79,28 @@ chrome.runtime.onConnect.addListener(function(port) {
         }
     });
 
-    let attemptedSignIn = false; // TODO: this is messy
     function doConversion(fileLines) {
-        checkCookies(doSendRequest, () => githubSignIn(doSendRequest));
+        let attemptedSignIn = false;
+        checkCookies(doSendRequest, () => {
+            attemptedSignIn = true;
+            githubSignIn(doSendRequest, () => {
+                sendErrorResponse({
+                    text: "sign-in tab closed, but no cookie found"
+                });
+            });
+        });
 
         function doSendRequest() {
             sendRequest(fileLines.join('\n'), sendSuccessResponse, (error) => {
                 if (error.status == 401 && !attemptedSignIn) {
                     // Attempt to sign in and try again.
+                    attemptedSignIn = true;
                     githubSignIn(doSendRequest);
                 } else {
                     sendErrorResponse(error);
                 }
             });
         }
-    }
-
-    function checkCookies(onSuccess, onFailure) {
-        chrome.cookies.get({
-            url: serverURL,
-            name: 'user'
-        }, (result) => {
-            console.log(result);
-            if (result) {
-                onSuccess();
-            } else {
-                onFailure && onFailure();
-            }
-        });
-    }
-
-    function githubSignIn(onSuccess) {
-        attemptedSignIn = true;
-        const loginWindow = window.open(`${serverURL}/login`);
-        const checkWindowClosed = setInterval(() => {
-            if (!loginWindow || loginWindow.closed) {
-                clearInterval(checkWindowClosed);
-                checkCookies(onSuccess);
-            }
-        }, 200);
     }
 
 });
