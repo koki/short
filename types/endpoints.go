36c90b2cb7445b19fb7ac7f6c8ef0ee4dd757de9
wlan0 <sidharthamn@gmail.com>
2017-12-01 23:44:51 -0800

    add types for endpoint

--- types/endpoints.go
+++ types/endpoints.go
@@ -1,6 +1,6 @@
 package types
 
-/*type EndpointWrapper struct {
+type EndpointWrapper struct {
 	Endpoint Endpoint `json:"endpoint,omitempty"`
 }
 
@@ -15,8 +15,14 @@ type EndpointSubset struct {
 }
 
 type EndpointAddress struct {
-	IP       string
-	Hostname string
-	Nodename *string
-	Target
-}*/
+	IP       string           `json:"ip,omitempty"`
+	Hostname string           `json:"hostname,omitempty"`
+	Nodename *string          `json:"node,omitempty"`
+	Target   *ObjectReference `json:"target,omitempty"`
+}
+
+type EndpointPort struct {
+	Name     string   `json:"name,omitempty"`
+	Port     int32    `json:"port,omitempty"`
+	Protocol Protocol `json:"protocol,omitempty"`
+}
---
+++ types/objectRef.go
@@ -0,0 +1,11 @@
+package types
+
+type ObjectReference struct {
+	Kind            string `json:"kind,omitempty"`
+	Namespace       string `json:"namespace,omitempty"`
+	Name            string `json:"name,omitempty"`
+	UID             string `json:"uid,omitempty"`
+	APIVersion      string `json:"version,omitempty"`
+	ResourceVersion string `json:"resource_version,omitempty"`
+	FieldPath       string `json:"field_path,omitempty"`
+}
