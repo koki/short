c24fdc53dee774024fd1edbe91ddd4d0d47da744
hitchhiker-vogon <hitchhiker.vogon@gmail.com>
2018-05-17 21:21:32 -0700

    patchStrategy, patchMergeKey and protobuf tags removed from LabelSelector and LabelSelectorRequirement

--- types/wh_config.go
+++ types/wh_config.go
@@ -49,14 +49,14 @@ type WebhookRuleWithOperations struct {
 }
 
 type LabelSelector struct {
-	MatchLabels      map[string]string          `json:"matchLabels,omitempty" protobuf:"bytes,1,rep,name=matchLabels"`
-	MatchExpressions []LabelSelectorRequirement `json:"matchExpressions,omitempty" protobuf:"bytes,2,rep,name=matchExpressions"`
+	MatchLabels      map[string]string          `json:"matchLabels,omitempty"`
+	MatchExpressions []LabelSelectorRequirement `json:"matchExpressions,omitempty"`
 }
 
 type LabelSelectorRequirement struct {
-	Key      string   `json:"key" patchStrategy:"merge" patchMergeKey:"key" protobuf:"bytes,1,opt,name=key"`
-	Operator string   `json:"operator" protobuf:"bytes,2,opt,name=operator,casttype=LabelSelectorOperator"`
-	Values   []string `json:"values,omitempty" protobuf:"bytes,3,rep,name=values"`
+	Key      string   `json:"key"`
+	Operator string   `json:"operator"`
+	Values   []string `json:"values,omitempty"`
 }
 
 func (i *WebhookRuleWithOperations) UnmarshalJSON(data []byte) error {
